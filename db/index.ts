66275c1e3790803f786adc4ae5ead32feda96579
EzyhB <ezyhbmail@gmail.com>
2022-04-04 12:10:39 +0100

    connected DB using PG and created pokemon_data Table

---
+++ db/index.ts
@@ -0,0 +1,14 @@
+import { Pool } from "pg";
+
+const connectionString = process.env.PG_CONNECTIONSTRING;
+
+const pool = new Pool({
+  connectionString,
+  ssl: { rejectUnauthorized: false },
+});
+
+const query = (text: string, params?: (string | number | boolean)[]) => {
+  return pool.query(text, params);
+};
+
+export default query;
